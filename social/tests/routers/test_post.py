f5282f5633b52ec9dd770819bb9243ed68b8dddd
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-10-05 12:33:04 +0200

    Add test for get comments on post endpoint

--- social/tests/routers/test_post.py
+++ social/tests/routers/test_post.py
@@ -72,3 +72,12 @@ async def test_create_comment(async_client: AsyncClient, created_post: dict):
 
     assert response.status_code == 201
     assert {"id": 0, "body": body}.items() <= response.json().items()
+
+
+@pytest.mark.anyio
+async def test_get_comments_on_post(
+    async_client: AsyncClient, created_post: dict, created_comment: dict
+):
+    response = await async_client.get(f"post/{created_post['id']}/comment")
+    assert response.status_code == 200
+    assert response.json() == [created_comment]
