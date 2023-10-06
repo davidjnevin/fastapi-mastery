e9834a60caacaa5277a89e600ef5fb92db8490a5
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-10-06 10:10:00 +0200

    Add configuration caching and get_config

--- social/config.py
+++ social/config.py
@@ -1,30 +1,43 @@
+from functools import lru_cache
 from typing import Optional
 
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class BaseConfig(BaseSettings):
-    env_file: str = ".env"
+    ENV_STATE: Optional[str] = None
+
+    model_config = SettingsConfigDict(env_file=".env")
 
 
-class GlobalConfig(BaseSettings):
+class GlobalConfig(BaseConfig):
     DATABASE_URL: Optional[str] = None
     DB_FORCE_ROLLBACK: bool = False
 
 
 class DevConfig(GlobalConfig):
-    class Config:
-        env_prefix = "DEV_"
+    model_config = SettingsConfigDict(env_prefix="DEV_")
 
 
 class ProdConfig(GlobalConfig):
-    class Config:
-        env_prefix = "PROD_"
+    model_config = SettingsConfigDict(env_prefix="PROD_")
 
 
 class TestConfig(GlobalConfig):
     DATABASE_URL: Optional[str] = "sqlite:///test.db"
     DB_FORCE_ROLLBACK: bool = True
 
-    class Config:
-        env_prefix = "TEST_"
+    model_config = SettingsConfigDict(env_prefix="TEST_")
+
+
+@lru_cache()
+def get_config(env_state: str) -> BaseConfig:
+    configs = {
+        "dev": DevConfig,
+        "prod": ProdConfig,
+        "test": TestConfig,
+    }
+    return configs[env_state]()
+
+
+config = get_config(BaseConfig().ENV_STATE)
