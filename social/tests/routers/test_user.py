0646938fe4260ab800e5516c27d5da1c3451c78c
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-10-12 01:25:29 +0200

    Use create_user and add exiting user test

--- social/tests/routers/test_user.py
+++ social/tests/routers/test_user.py
@@ -2,27 +2,29 @@ import pytest
 from httpx import AsyncClient
 
 
-async def create_user(
-    email: str, password: str, async_client: AsyncClient
-) -> dict:
-    response = await async_client.post(
+async def create_user(email: str, password: str, async_client: AsyncClient):
+    return await async_client.post(
         "/register",
         json={"email": email, "password": password},
     )
-    return response.json()
 
 
 @pytest.mark.anyio
-async def test_register(async_client: AsyncClient):
+async def test_register_user(async_client: AsyncClient):
     email = "bob@davidnevin.net"
     password = "password"
 
-    response = await async_client.post(
-        "/register",
-        json={
-            "email": email,
-            "password": password,
-        },
-    )
+    response = await create_user(email, password, async_client)
     assert response.status_code == 201
     assert {"detail": "User created."}.items() <= response.json().items()
+
+
+@pytest.mark.anyio
+async def test_register_user_with_existing_email(
+    async_client: AsyncClient, registered_user: dict
+):
+    response = await create_user(
+        registered_user["email"], registered_user["password"], async_client
+    )
+    assert response.status_code == 400
+    assert "already registered" in response.json()["detail"]
