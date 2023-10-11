0dbb0d9bdbd7a1698c1e9a97772988209c37428a
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-10-12 00:33:40 +0200

    Add user register endpoint

---
+++ social/routers/user.py
@@ -0,0 +1,26 @@
+import logging
+
+from fastapi import APIRouter, HTTPException
+
+import social.security as security
+from social.database import database, user_table
+from social.models.user import UserIn
+
+router = APIRouter()
+logger = logging.getLogger(__name__)
+
+
+@router.post("/register", status_code=201)
+async def register(user: UserIn) -> dict:
+    logger.info("Creating user")
+    if await security.get_user(user.email):
+        raise HTTPException(
+            status_code=400,
+            detail="A user with that email already registered",
+        )
+    query = user_table.insert().values(
+        email=user.email, password=user.password
+    )
+    logger.debug(query)
+    await database.execute(query)
+    return {"detail": "User created."}
---
+++ social/tests/routers/test_user.py
@@ -0,0 +1,29 @@
+import pytest
+from httpx import AsyncClient
+
+
+async def create_user(
+    email: str, password: str, async_client: AsyncClient
+) -> dict:
+    response = await async_client.post(
+        "/register",
+        json={"email": email, "password": password},
+    )
+    return response.json()
+
+
+@pytest.mark.anyio
+async def test_register(async_client: AsyncClient):
+    email = "bob@davidnevin.net"
+    password = "password"
+
+    response = await async_client.post(
+        "/register",
+        json={
+            "email": email,
+            "password": password,
+        },
+    )
+    breakpoint()
+    assert response.status_code == 201
+    assert {"detail": "User created."}.items() <= response.json().items()
