38d07725553235bb0e7a9cd5bdb62d503b2e0d5f
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-10-12 11:59:00 +0200

    Add get hash and verify hash functions

--- social/security.py
+++ social/security.py
@@ -1,16 +1,26 @@
 import logging
 
-import social.database
+from passlib.context import CryptContext
+
+from social.database import database, user_table
 
 logger = logging.getLogger(__name__)
+
+pwd_context = CryptContext(schemes=["bcrypt"])
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password, hashed_password) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
 
 
 async def get_user(email: str):
     logger.debug("Getting user form the database", extra={"email": email})
-    query = social.database.user_table.select().where(
-        social.database.user_table.c.email == email
-    )
+    query = user_table.select().where(user_table.c.email == email)
     logger.debug(query)
-    result = await social.database.database.fetch_one(query)
+    result = await database.fetch_one(query)
     if result:
         return result
