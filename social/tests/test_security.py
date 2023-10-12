58e434c6345f7d663234d6ba7ebb58f651848869
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-10-12 11:59:38 +0200

    Add hashing test - marked as skip

--- social/tests/test_security.py
+++ social/tests/test_security.py
@@ -6,6 +6,13 @@ from databases.interfaces import Record
 from social import security
 
 
+@pytest.mark.skip(reason="Slow test")
+def test_password_hashes():
+    password = "secret"
+    hashed_password = security.get_password_hash(password)
+    assert security.verify_password(password, hashed_password)
+
+
 @pytest.mark.anyio
 async def test_get_user(registered_user: dict):
     user: Optional[Record] = await security.get_user(registered_user["email"])
@@ -13,7 +20,7 @@ async def test_get_user(registered_user: dict):
         pytest.fail("User not found")
     assert user.email == registered_user["email"]
     assert user.id == registered_user["id"]
-    # assert user["password"] != registered_user["password"]
+    assert user.password != registered_user["password"]
 
 
 @pytest.mark.anyio
