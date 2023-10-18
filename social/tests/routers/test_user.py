bfc4727490c621dcfc0e97652042ccf4112f3c15
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-10-18 12:39:47 +0200

    Add user login and register tests

--- social/tests/routers/test_user.py
+++ social/tests/routers/test_user.py
@@ -28,3 +28,26 @@ async def test_register_user_with_existing_email(
     )
     assert response.status_code == 400
     assert "already registered" in response.json()["detail"]
+
+
+@pytest.mark.anyio
+async def test_register_user_with_invalid_email(async_client: AsyncClient):
+    response = await async_client.post(
+        "/login", json={"email": "bob", "password": "password"}
+    )
+    assert response.status_code == 401
+
+
+@pytest.mark.anyio
+async def test_login_user(async_client: AsyncClient, registered_user: dict):
+    response = await async_client.post(
+        "/login",
+        json={
+            "email": registered_user["email"],
+            "password": registered_user["password"],
+        },
+    )
+    assert response.status_code == 200
+    assert "access_token" in response.json()
+    assert "token_type" in response.json()
+    assert response.json()["token_type"] == "bearer"
