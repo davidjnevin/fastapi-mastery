2bdcb144e75045fc680af220146d8480933a881f
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-10-18 12:38:38 +0200

    Add authenucate_user and supporting functions

--- social/security.py
+++ social/security.py
@@ -1,12 +1,37 @@
+import datetime
 import logging
 
+import fastapi
+from jose import jwt
 from passlib.context import CryptContext
 
+from social.config import config
 from social.database import database, user_table
 
 logger = logging.getLogger(__name__)
 
+JWT_SECRET = config.JWT_SECRET_KEY
+ALGORITH = config.JWT_ALGORITHM
 pwd_context = CryptContext(schemes=["bcrypt"])
+
+credentials_exception = fastapi.HTTPException(
+    status_code=fastapi.status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+)
+
+
+def access_token_expire_minutes():
+    return 30
+
+
+def create_access_token(email: str):
+    logger.debug("Creating access token", extra={"email": email})
+    expire = datetime.datetime.now(datetime.UTC) + datetime.timedelta(
+        minutes=access_token_expire_minutes()
+    )
+    jwt_data = {"sub": email, "exp": expire}
+    encoded_jwt = jwt.encode(jwt_data, key=JWT_SECRET, algorithm=ALGORITH)
+    return encoded_jwt
 
 
 def get_password_hash(password: str) -> str:
@@ -24,3 +49,14 @@ async def get_user(email: str):
     result = await database.fetch_one(query)
     if result:
         return result
+    return None
+
+
+async def authenticate_user(email: str, password: str):
+    logger.debug("Authenticating user", extra={"email": email})
+    user = await get_user(email)
+    if not user:
+        raise credentials_exception
+    if not verify_password(password, user["password"]):
+        raise credentials_exception
+    return user
