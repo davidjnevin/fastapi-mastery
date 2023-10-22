0b876d355ce493c1474fa03f74d6394e257db41d
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-10-22 12:18:06 +0200

    Add sorting paramater to get_all_posts

--- social/routers/post.py
+++ social/routers/post.py
@@ -1,4 +1,5 @@
 import logging
+from enum import Enum
 from typing import Annotated
 
 import sqlalchemy
@@ -52,10 +53,21 @@ async def create_post(
     return {**data, "id": last_record_id}
 
 
-@router.get("/post", response_model=list[UserPost])
-async def get_all_posts():
+class PostSorting(str, Enum):
+    new = "new"
+    old = "old"
+    most_likes = "most_likes"
+
+
+@router.get("/post", response_model=list[UserPostWithLikes])
+async def get_all_posts(sorting: PostSorting = PostSorting.new):
     logger.info("Getting all posts")
-    query = post_table.select()
+    if sorting == PostSorting.new:
+        query = select_post_with_likes.order_by(post_table.c.id.desc())
+    elif sorting == PostSorting.old:
+        query = select_post_with_likes.order_by(post_table.c.id.asc())
+    elif sorting == PostSorting.most_likes:
+        query = select_post_with_likes.order_by(sqlalchemy.desc("likes"))
     logger.debug(query)
     return await database.fetch_all(query)
 
