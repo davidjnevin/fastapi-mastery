507a37981c6a5c2e91eec21dc7910e5a45bba622
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-10-22 10:45:22 +0200

    Add like_table

--- social/models/post.py
+++ social/models/post.py
@@ -12,6 +12,11 @@ class UserPost(UserPostIn):
     user_id: int
 
 
+class UserPostWithLikes(UserPost):
+    model_config = ConfigDict(from_attributes=True)
+    likes: int
+
+
 class CommentIn(BaseModel):
     body: str
     post_id: int
@@ -25,7 +30,7 @@ class Comment(CommentIn):
 
 
 class UserPostWithComments(BaseModel):
-    post: UserPost
+    post: UserPostWithLikes
     comments: list[Comment]
 
 
