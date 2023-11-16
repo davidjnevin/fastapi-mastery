79cca6f98e9f076d688dcea9354453039520a254
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-11-16 11:42:57 +0100

    Refactor logging and response messages

--- social/tasks.py
+++ social/tasks.py
@@ -7,12 +7,12 @@ from social.config import config
 logger = logging.getLogger(__name__)
 
 
-class ApiResponseException(Exception):
+class APIResponseException(Exception):
     pass
 
 
 async def send_simple_email(to: str, subject: str, body: str):
-    logger.debug(f"Sending email to {to[:3]} with subject {subject}")
+    logger.debug(f"Sending email to '{to[:3]}' with subject '{subject[:10]}'")
     async with httpx.AsyncClient() as client:
         try:
             response = await client.post(
@@ -27,21 +27,21 @@ async def send_simple_email(to: str, subject: str, body: str):
             )
             response.raise_for_status()
             logger.debug(response.content)
+            logger.debug(f"Email sent to {to[:3]} with subject {subject}")
+            return response
 
         except httpx.HTTPStatusError as e:
             logger.error(f"Error sending email: {e}")
-            raise ApiResponseException(
+            raise APIResponseException(
                 f"API request with status code {e.response.status_code} failed"
             ) from e
-
-        logger.debug(f"Email sent to {to[:3]} with subject {subject}")
-        return response.json()
 
 
 async def send_user_registration_email(to: str, confirmation_url: str):
     subject = "Please confirm your email"
     body = f"""
     Hi there,
+    You have successfully registered signed up!
     Please confirm your email by clicking on the link below:
     {confirmation_url}
     """
