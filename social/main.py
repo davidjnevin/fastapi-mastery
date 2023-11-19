a8c407bfb1611fcd227fe35b92eab8ec7d18cb7a
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-11-19 22:10:11 +0100

    Add sentry monitoring

--- social/main.py
+++ social/main.py
@@ -3,13 +3,27 @@ from contextlib import asynccontextmanager
 
 import fastapi
 import fastapi.exception_handlers
+import sentry_sdk
 from asgi_correlation_id import CorrelationIdMiddleware
 
+from social.config import config
 from social.database import database
 from social.logging_conf import configure_logging
 from social.routers import healthcheck, post, upload, user
 
 logger = logging.getLogger(__name__)
+
+
+sentry_sdk.init(
+    dsn=config.SENTRY_DSN,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 
 
 @asynccontextmanager
@@ -27,6 +41,7 @@ app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(healthcheck.router)
 app.include_router(upload.router)
+# app.include_router(sentry.router)
 
 
 @app.exception_handler(fastapi.HTTPException)
